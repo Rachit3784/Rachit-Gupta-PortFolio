996d0acd1c3bc0b2fa5a64834c04ac9f0ef7e7e4
Rachit3784 <grachit736@gmail.com>
2026-08-02 02:32:04 +0530

    commit

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,11 +1,16 @@
 // src/lib/socket.js
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:5000';
+const SOCKET_URL =
+  process.env.NEXT_PUBLIC_SOCKET_URL ||
+  process.env.NEXT_PUBLIC_SERVER_URL ||
+  'http://localhost:5000';
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
   reconnection: true,
-  reconnectionAttempts: 10,
+  reconnectionAttempts: 20,
   reconnectionDelay: 1000,
+  transports: ['websocket', 'polling'],
+  secure: true,
 });
