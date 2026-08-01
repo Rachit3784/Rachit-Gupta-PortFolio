114f7bd0cf2c413749d75ad50290ce9e2a0bab7f
Rachit3784 <grachit736@gmail.com>
2026-08-02 01:38:38 +0530

    fix

--- src/app/bot/route.js
+++ src/app/bot/route.js
@@ -8,7 +8,7 @@ export async function POST(request) {
     if (!message) {
       return NextResponse.json(
         { error: 'Information is required' },
-        { status: 400 }
+        { status: 400}
       );
     }
 
